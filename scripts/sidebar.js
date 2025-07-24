943fb064ac47ae50cf0bd1497ad83937ea205efa
fymin-ui <yagubzade2005@gmail.com>
2025-07-24 17:19:51 +0400

    logout part is ready

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,12 +2,19 @@ const toggleBtn = document.getElementById("toggle-btn");
 const sidebar = document.getElementById("sidebar");
 const main = document.getElementById("main");
 
-toggleBtn.addEventListener("click", () => {
-  if (sidebar.style.left === "0px") {
-    sidebar.style.left = "-250px";
-    main.style.marginLeft = "0";
-  } else {
-    sidebar.style.left = "0px";
-    main.style.marginLeft = "250px";
-  }
-});
+//for the logout part
+const logout = document.getElementById("logout");
+
+function toggleButton(){
+  sidebar.classList.toggle("hidden");
+  main.classList.toggle("shifted");
+}
+
+toggleBtn.addEventListener("click", toggleButton);
+
+function confirmation(){
+  const confirmIt = confirm("Do you really want to log out?");
+
+}
+
+logout.addEventListener("click", confirmation);
